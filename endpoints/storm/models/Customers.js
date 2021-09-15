79fa3a7518a91fddfc2cd569ccda546a45da48c9
Naman Aggarwal <aggarwal.nam@gmail.com>
2021-09-15 22:19:15 +0100

    Added get customers by type

--- endpoints/storm/models/Customers.js
+++ endpoints/storm/models/Customers.js
@@ -1,10 +1,19 @@
 module.exports =  function Customers(database) {
   const TABLE_NAME = 'customers';
+  const USERS_COL = [
+    'first_name',
+    'last_name',
+    'avatar',
+  ]
 
   this.getCustomersByType = async function() {
     return database(TABLE_NAME)
     .select()
-    .leftJoin('directus_users as u', `${TABLE_NAME}.user_created`, 'u.id' );
+    .innerJoin('directus_users as cu', `${TABLE_NAME}.user_created`, 'cu.id' )
+    .innerJoin('directus_users as uu', `${TABLE_NAME}.user_updated`, 'uu.id' )
+    .innerJoin('directus_users as ui', `${TABLE_NAME}.user_incharge`, 'ui.id' )
+    .select(['cu.first_name as created_customer', 'uu.first_name as updated_user', 'ui.first_name'])
+    .limit(1);
   }
 
   this.createNewCustomer = async function(data) {
